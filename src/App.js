aea74cf35ef4f6b37d1f6db3f727cdbd54d35922
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2021-02-01 18:07:06 +0900

    test commited0201

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import fetch from 'node-fetch';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state ={
+      text: '',
+      left: '',
+      right: '',
+      total: ''
+    }
+    
+  }
+
+  componentDidMount(){
+    const URL = 'http://localhost:8080/getResult'
+    fetch(URL, {mode:'cors'})
+    .then(res =>res.json())
+    .then(json =>{
+      console.log(json)
+      this.setState({
+        text: "計算結果の表示：",
+        left: json[0]['left'] + ' + ',
+        right: json[0]['right'] + ' = ',
+        total: json[0]['total']
+      })
+    })
+  }
+
+  
+  render(){
+    /*
+    return(
+      <div className="results">
+        <form action="http://localhost:8080/test" method = "post">
+          <input type="text" name="leftNumber" placeholder="足される数"/>+
+          <input type="text" name="rightNumber" placeholder="足す数"/>=<br />
+          {this.state.text} {this.state.left} {this.state.right} {this.state.total}<br />
+          <button type="submit">計算</button>
+        </form> 
+      </div>
+    )
+    */
+   return(
+     <div className='App'>
+       {this.state.text} {this.state.left} {this.state.right} {this.state.total}
+     </div>
+   )
+  }
 }
 
 export default App;
